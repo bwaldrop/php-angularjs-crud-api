32ad80ac931997e5e7799bc4bab5db4a259b9615
Bradley Waldrop <bwaldrop@yahoo.com>
2016-05-13 10:34:27 -0700

    initial commit

---
+++ phpajaxcrudlevel1_DB_create.sql
@@ -0,0 +1,13 @@
+--
+-- Table structure for table `products`
+--
+ 
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
